d57660bd412b991ff1a3607104e3d425cbb2436b
user <info@ethicalreporting.org>
2018-01-23 23:32:27 -0500

    pypi versioning

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'wifiEssentials',
-    version = '1.0.0',
+    version = '1.0.2',
     author = 'stryngs',
     author_email = 'info@ethicalreporting.org',
     packages = ['wifiEssentials', 'wifiEssentials.lib'],
