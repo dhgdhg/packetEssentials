24012146e280c83f24fdabe00c68880677d7169a
stryngs <info@ethicalreporting.org>
2019-04-29 17:54:24 +0000

    Modified for Windows usage

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'packetEssentials',
-    version = '1.2.6',
+    version = '1.2.8',
     author = 'stryngs',
     author_email = 'info@ethicalreporting.org',
     packages = ['packetEssentials', 'packetEssentials.lib'],
